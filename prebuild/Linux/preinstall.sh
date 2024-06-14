b114bf0d5d932eb994217bdb469d406169d6e1da
Kai Rieffel <krieffel@nutshelllabs.io>
2024-06-15 03:57:23 +0700

    Testing

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# linux build is done on a docker image that has them
-
-# git clone git://anongit.gentoo.org/proj/pax-utils.git
-# cd pax-utils
-# PATH=$PATH:$PWD
-# make
-# cd ..
-
 echo "Preinstall steps complete"
